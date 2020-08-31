044c45685dfb7f89a066ff2addb48510a4439224
WonderBGK <55256977+WonderBGK@users.noreply.github.com>
2020-08-31 05:11:01 +0200

    Add files via upload
    hope it works

---
+++ Assets/ButtonClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    public GameObject newsB;
+
+
+    public void OpenNoticeboard() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenNews() {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+
+
+}
