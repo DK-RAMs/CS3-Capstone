904cab494024ca380c7cd666fab127840776c9f1
WonderBGK <55256977+WonderBGK@users.noreply.github.com>
2020-09-23 12:57:22 +0200

    Add files via upload

--- Assets/Navigation.cs
+++ Assets/Navigation.cs
@@ -6,6 +6,21 @@ using UnityEngine.SceneManagement;
 public class Navigation : MonoBehaviour
 {
     public void GoBack(){
-            SceneManager.LoadScene("Scene1");
+            SceneManager.LoadScene("Scene4");
         }
+
+    public void OpenNoticeboard() {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+    public void OpenNews() {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Quit(){
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+
 }
